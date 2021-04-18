9389d8e3a6529e303b5e833b8d2dc2a0b8913cd2
Termonoid <termonoid@yandex.ru>
2021-04-18 11:30:25 +0300

    Autoformatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
+use rand::seq::SliceRandom;
 use std::collections::HashMap;
 use std::convert::{TryFrom, TryInto};
-use rand::seq::SliceRandom;
 
 #[cfg(test)]
 mod tests {
@@ -9,37 +9,38 @@ mod tests {
     fn test() {
         let mut g = Generator::new();
         g.train("some stupid words to test some stupid code");
-        println!("{}",g.generate(20))
+        println!("{}", g.generate(20))
     }
 }
 type Node = Option<Box<str>>;
 type Key = [Node; 2]; //len should always be 2
 
 #[derive(Default)]
-pub struct Generator{
+pub struct Generator {
     text: String,
-    chain: HashMap<Key, Vec<Node>>
+    chain: HashMap<Key, Vec<Node>>,
 }
-impl Generator{
+impl Generator {
     pub fn new() -> Self {
         Generator {
             text: String::new(),
-            chain: HashMap::new()
+            chain: HashMap::new(),
         }
     }
-    pub fn train(&mut self, text: &str){
-        if text.is_empty(){
-            return
+    pub fn train(&mut self, text: &str) {
+        if text.is_empty() {
+            return;
         }
         if text.contains('\n') {
             for s in text.split('\n') {
                 self.train(s)
             }
-            return
+            return;
         }
         self.text.push_str(text);
         self.text.push('\n');
-        let mut text = text.split_whitespace()
+        let mut text = text
+            .split_whitespace()
             .map(String::from)
             .map(String::into_boxed_str)
             .map(Option::from)
@@ -50,40 +51,39 @@ impl Generator{
         for window in text.windows(3) {
             let (key, value) = window.split_at(2);
             match self.chain.get_mut(key) {
-                Some(vector) => { vector.extend_from_slice(value) },
-                None         => {
+                Some(vector) => vector.extend_from_slice(value),
+                None => {
                     let key = key.to_owned().try_into().unwrap();
-                    self.chain.insert(key, value.to_vec()); }
-
+                    self.chain.insert(key, value.to_vec());
+                }
             };
         }
-        
     }
-    pub fn generate(&self, tries:usize) -> String {
+    pub fn generate(&self, tries: usize) -> String {
         if tries == 0 {
             return String::from("хуй тебе");
         }
         let mut rng = rand::thread_rng();
         let mut string: Vec<Node> = vec![None, None];
         loop {
-            let index = &string[string.len()-2..];
+            let index = &string[string.len() - 2..];
             let variants = self.chain.get(index).unwrap();
             let choice = variants.choose(&mut rng).unwrap().clone();
             if choice.is_none() {
-                break
+                break;
             }
             string.push(choice);
         }
-        let result = string.into_iter()
+        let result = string
+            .into_iter()
             .skip(2)
-            .map( Option::unwrap )
+            .map(Option::unwrap)
             .collect::<Vec<_>>()
             .join(" ");
         if !self.text.contains(&result) {
             result
-        }
-        else {
-            self.generate(tries-1)
+        } else {
+            self.generate(tries - 1)
         }
     }
 }
